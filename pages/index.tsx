dfe884b87d5ecf81aac933b88c0107a9ba2c3df3
Brandon Lok (Qi Luo) <branlok95@gmail.com>
2023-01-10 12:43:19 -0500

    .

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,11 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
+import client from '../utils/sanityApi'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home({ name }) {
+export default function Home({ name }: { name: string }) {
   return (
     <>
       <Head>
