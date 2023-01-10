ac34554ddd1c3e6276454cc02b16763da56a0cde
Brandon Lok (Qi Luo) <branlok95@gmail.com>
2023-01-10 12:41:20 -0500

    added sanity js api client

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import styles from '../styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function Home({ name }) {
   return (
     <>
       <Head>
@@ -26,7 +26,7 @@ export default function Home() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              By{' '}
+              By{' '}  {name}
               <Image
                 src="/vercel.svg"
                 alt="Vercel Logo"
@@ -120,4 +120,11 @@ export default function Home() {
       </main>
     </>
   )
+}
+
+export async function getStaticProps(context) {
+
+  return {
+    props: { name: 'brandon' }, // will be passed to the page component as props
+  }
 }
---
+++ utils/sanityAPI.ts
@@ -0,0 +1,12 @@
+const sanityClient = require('@sanity/client')
+const TOKEN = process.env.TOKEN;
+
+const client = sanityClient({
+  projectId: '24jcy0ne',
+  dataset: 'production',
+  apiVersion: '2023-01-10', // use current UTC date - see "specifying API version"!
+  token: TOKEN, // or leave blank for unauthenticated usage
+  useCdn: false, // `false` if you want to ensure fresh data
+})
+
+export default client
